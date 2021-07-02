e1a0769be4eaa0272883d219e8e5b49bb8e6ce13
sirrrik <you@example.com>
2021-07-02 11:03:22 +0300

    follow button changed

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button class="btn btn-primary ml-4" @click="followUser" v-text="buttonText"></button>
+        <button class="btn btn-primary ml-4" @click="followUser" v-text="buttonText">follow</button>
     </div>
 </template>
 
